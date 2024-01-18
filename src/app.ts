184ac944de7efe9a8a7a750fa43e82a565cd49b8
Martin Router King <Alex110397@gmail.com>
2024-01-18 18:10:28 +0100

    WIP - 1.0.2

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
-import express ,{Response,Request} from 'express';
+import express, { Response, Request } from 'express';
 import * as dotenv from 'dotenv';
 dotenv.config();
+import fs from 'fs';
 
 import helmet from 'helmet';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 
-import {googleRouter} from './router'
+import { googleRouter } from './router'
 
 const app = express();
-const port:number = parseInt(process.env.PORT as string,10);
-const host:string = String(process.env.HOST_DEV);
+const port: number = parseInt(process.env.PORT as string, 10);
+const host: string = String(process.env.HOST_DEV);
 
 app.use(helmet());
 app.use(cors());
@@ -18,10 +19,14 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.route("/").get((req:Request,res:Response)=>{
-    res.json("Test received!")
+app.route("/").get((req: Request, res: Response) => {
+  fs.readFile('./index.html', 'utf8', (err, text) => {
+    // pour le cross origin
+    res.set("Content-Security-Policy", "default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'")
+      .send(text);
+  });
 })
 app.use(`/`, googleRouter);
-app.listen(port,()=>{
-    console.log(`Server is running at ${host}:${port} !`);
+app.listen(port, () => {
+  console.log(`Server is running at ${host}:${port} !`);
 })
--- src/router.ts
+++ src/router.ts
@@ -1,17 +1,18 @@
 import express, { Request, Response } from 'express';
 import { ConvertHtmlToDocxService } from './Service/googleService'
+const { Readable } = require('stream');
+import fs from 'fs';
 
 export const googleRouter = express.Router();
-
 googleRouter.post('/', (async (req: Request, res: Response) => {
   const html = req.body.html;
   try {
     const item = await ConvertHtmlToDocxService(html);
-    res.status(200).json({
-      status: 'Successful',
-      // @ts-ignore
-      data: new Buffer.from(item, 'binary')
-    })
+    res.setHeader('Content-disposition', 'attachment; filename=' + 'document.docx');
+    res.setHeader('Content-type', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document');
+    // @ts-ignore
+    res.setHeader('Content-Length', item.length)
+    return res.end(item)
   }
   catch (err) {
     res.status(503).json({
